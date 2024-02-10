8137feda7ebbdf0a800a460d6667d227b8214406
sohailmomin1391 <140325105+itsmesam59@users.noreply.github.com>
2024-02-10 22:29:07 +0530

    ...

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
 
 let bill_total = document.getElementById('bill-total');
 let total = document.getElementById('total');
-let count = document.getElementById('count-btn')
+let tip = document.getElementById('tip');
+let persons = document.getElementById('persons');
 
-count.onclick = () => {
-    calculatebill();
+// Buttons
+let minus = document.getElementById('minus');
+let plus = document.getElementById('plus');
+
+
+let billtotal = 0;
+let tip_amount = 0;
+let total_amount = 0;
+let people = 1;
+
+
+minus.onclick = () => {
+    if (people > 1) {
+        people --;
+        persons.innerHTML=`<p>${people}</p>`
+    }
+    calculate();
 }
 
-const calculatebill = () => {
-    total.innerHTML = `<p>${bill_total.value}</p>`;
+plus.onclick = () => {
+    people ++;
+    persons.innerHTML = `<p>${people}</p>`;
+    calculate();
+}
+
+const calculate = () =>{
+    tip_amount = Number(tip.value / 100);
+    billtotal = Number(bill_total.value);
+    total_amount = billtotal + (billtotal * tip_amount) ;
+    total.innerHTML = `<p>$${total_amount * people}</p>`;
 }
